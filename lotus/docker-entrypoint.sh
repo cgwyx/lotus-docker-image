f580c5bb2e28083f32616d10867194fc04e1be58
Ning Yao <zay11022@163.com>
2020-05-26 17:16:20 +0800

    Update docker-entrypoint.sh

--- lotus/docker-entrypoint.sh
+++ lotus/docker-entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 : ${CUSTOM_GPU:="GeForce RTX 2080 Ti:4352"}
+: ${ADDRESS:="127.0.0.1:2345"}
+: ${daemon:="seal-worker"}
 
 mkdir -p /storage/lotuswork/lotusstorage \
   && mkdir -p /storage/lotuswork/lotus \
@@ -10,7 +12,14 @@ mkdir -p /storage/lotuswork/lotusstorage \
 
 export BELLMAN_CUSTOM_GPU=${CUSTOM_GPU}
 
+if [ x"$daemon" == x"seal-worker" ];then
+  lotus-seal-worker run --address ${ADDRESS}
+elif [x"$dameon" == x"miner-storage" ];then
+  lotus-storage-miner run
+elif [x"$daemon" == x"dev"];then
 while :
 do
     sleep 3600
 done
+fi
+
