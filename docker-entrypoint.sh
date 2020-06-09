aa55a67860fea0bf2ff4c0620d1be045562853f5
Ning Yao <yaoning@unitedstack.com>
2020-06-09 22:12:00 +0800

    add cmd exection feature in docker-entrypoint.sh
    Signed-off-by: Ning Yao <yaoning@unitedstack.com>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,8 +3,9 @@
 : ${BELLMAN_CUSTOM_GPU:="GeForce RTX 2080 Ti:4352"}
 : ${FIL_PROOFS_MAXIMIZE_CACHING:=1}
 : ${ADDRESS:="127.0.0.1:2345"}
-: ${daemon:="seal-worker"}
+: ${daemon:="cmd"}
 : ${options:="--no-local-storage=false"}
+: ${cmd_cli:=""}
 
 mkdir -p /storage/lotuswork/lotusstorage \
   && mkdir -p /storage/lotuswork/lotus \
@@ -17,8 +18,10 @@ export FIL_PROOFS_MAXIMIZE_CACHING=${FIL_PROOFS_MAXIMIZE_CACHING}
 
 if [ x"$daemon" == x"seal-worker" ];then
   lotus-seal-worker run --address ${ADDRESS} ${options}
-elif [ x"$dameon" == x"miner-storage" ];then
+elif [ x"$dameon" == x"storage-miner" ];then
   lotus-storage-miner run ${options}
+elif [ x"$dameon" == x"cmd" ];then
+  ${cmd_cli} ${options}
 elif [ x"$daemon" == x"dev" ];then
   while :
   do
