c1ba4941c424b12309e4a0310ec1b6b2e3e41bd0
Ning Yao <yaoning@unitedstack.com>
2020-06-11 17:48:32 +0000

    fix typos in build.sh
    Signed-off-by: Ning Yao <yaoning@unitedstack.com>

--- build.sh
+++ build.sh
@@ -16,19 +16,19 @@ docker pull mslovy/lotus:intel
 docker pull mslovy/lotus:intel_gpu
 
 # building images
-if [ x"$TYPE" == x"interopnet" ]; then
-    docker build -t ${ADDRESS}:5000/lotus:intel -f interopnet/Dockerfile
-    docker build -t ${ADDRESS}:5000/lotus:intel_gpu -f interopnet_gpu/Dockerfile
-    if [ x"$AMD" == x"true" ]; then
+if [ x"$TYPE" = x"interopnet" ]; then
+    docker build -t ${ADDRESS}:5000/lotus:intel -f interopnet/Dockerfile .
+    docker build -t ${ADDRESS}:5000/lotus:intel_gpu -f interopnet_gpu/Dockerfile .
+    if [ x"$AMD" = x"true" ]; then
 	sed -i "s#docker.io/mslovy#${ADDRESS}:5000#" AMD/Dockerfile
 	sed -i "s#docker.io/mslovy#${ADDRESS}:5000#" AMD_gpu/Dockerfile
-        docker build -t ${ADDRESS}:5000/lotus:AMD -f AMD/Dockerfile 
-        docker build -t ${ADDRESS}:5000/lotus:AMD_gpu -f AMD_gpu/Dockerfile
+        docker build -t ${ADDRESS}:5000/lotus:AMD -f AMD/Dockerfile . 
+        docker build -t ${ADDRESS}:5000/lotus:AMD_gpu -f AMD_gpu/Dockerfile .
 	git checkout -f
     fi
 else
-    if [ x"$AMD" == x"true" ]; then
-	docker build -t ${ADDRESS}:5000/lotus:AMD -f AMD/Dockerfile 
-	docker build -t ${ADDRESS}:5000/lotus:AMD_gpu -f AMD_gpu/Dockerfile
+    if [ x"$AMD" = x"true" ]; then
+	docker build -t ${ADDRESS}:5000/lotus:AMD -f AMD/Dockerfile .
+	docker build -t ${ADDRESS}:5000/lotus:AMD_gpu -f AMD_gpu/Dockerfile .
     fi
 fi
